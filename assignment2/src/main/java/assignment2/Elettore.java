84526fda024aa4a69621c5b99b66a4443acc9099
Scogna00 <lorenzo.scognamiglio@studenti.unimi.it>
2021-11-03 16:17:36 +0100

    modifies Elettore

--- assignment2/src/main/java/assignment2/Elettore.java
+++ assignment2/src/main/java/assignment2/Elettore.java
@@ -2,4 +2,17 @@ package assignment2;
 
 public class Elettore {
 
+	List lista_candidati = new ArrayList();
+	
+	public static void setLista_candidati(String c) {
+		
+		lista_candidati.add(c); 
+	}
+	
+	public static List<String> getLista_candidati() {
+		
+		return lista_candidati;	
+	}
+	
+	String nome_elettore;
 }
