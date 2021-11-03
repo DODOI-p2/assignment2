a7709a32d1b82609ed734f48d009d10ac7bd11c1
DODOI-p2 <daniele.naldi@studenti.unimi.it>
2021-11-03 15:53:15 +0100

    modifies scrutatore.

--- assignment2/src/main/java/assignment2/Scrutatore.java
+++ assignment2/src/main/java/assignment2/Scrutatore.java
@@ -2,4 +2,20 @@ package assignment2;
 
 public class Scrutatore {
 
+	boolean fine_elezione;
+	
+	public void setFine_elezioni(int f) {
+		
+		fine_elezione=f; 
+		
+	} 
+	
+	public int getFine_elezioni() {
+		
+		return fine_elezione; }
+	
+	int conteggiovoti;
+	
+	string nomecandidato;
+	
 }
